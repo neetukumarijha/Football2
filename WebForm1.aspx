3f7f9ff86b6375e72bbf32bfc622dd09ee4ce527
neetu <neetukumarijha29@gmail.com>
2022-05-21 19:21:14 -0700

    first commit

---
+++ WebForm1.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Football.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Japan Data" Width="201px" />
+        </p>
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Win" Width="203px" />
+        <p>
+            <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Draw" Width="206px" />
+        </p>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Football
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          DataClasses1DataContext data = new DataClasses1DataContext();
+            GridView1.DataSource = data.tblFootBallLeagues;
+            GridView1.DataBind();
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DataClasses1DataContext data = new DataClasses1DataContext();
+
+            GridView1.DataSource = from football in data.tblFootBallLeagues
+                                   where football.TeamName1 == "Japan" || football.TeamName2 == "Japan"
+                                   select football;
+            GridView1.DataBind();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            NewMethod3();
+        }
+        private void NewMethod3()
+        {
+            DataClasses1DataContext data = new DataClasses1DataContext();
+
+            GridView1.DataSource = from football in data.tblFootBallLeagues
+                                   where football.MatchStatus == "Draw"
+                                   select football;
+            GridView1.DataBind();
+        }
+        private void Getdata()
+        {
+            DataClasses1DataContext dataClasses1DataContext = new DataClasses1DataContext();
+            GridView1.DataSource = dataClasses1DataContext.tblFootBallLeagues;
+            GridView1.DataBind();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            DataClasses1DataContext data = new DataClasses1DataContext();
+
+            GridView1.DataSource = from football in data.tblFootBallLeagues
+                                   where football.MatchStatus=="Win"
+                                   select football;
+            GridView1.DataBind();
+
+        }
+    }
+}
